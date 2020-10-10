4ece9c1739c6ba00fcfdbb7a033f2eb8f60c3350
juna <ju0830na17@gmail.com>
2020-10-10 12:53:18 +0900

    コミット

--- Original App/CountViewController.swift
+++ Original App/CountViewController.swift
@@ -23,8 +23,8 @@ class CountViewController: UIViewController {
     @IBOutlet var nichiLabel: UILabel!
     @IBOutlet var plusbutton: UIButton!
     @IBOutlet var minusbutton: UIButton!
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,37 +32,43 @@ class CountViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        eventLabel.text = saveData.object(forKey: "event") as? String
-        dayLabel.text = saveData.object(forKey: "day") as? String
-        contentTextView.text = saveData.object(forKey: "content") as? String
         
-        number2 = saveData.object(forKey: "day") as! Int
-    }
-    
-    //@IBAction func saveMemo(){
+        if saveData.object(forKey: "day") as? String != nil {
+            
+            eventLabel.text = saveData.object(forKey: "event") as? String
+            dayLabel.text = saveData.object(forKey: "day") as? String
+            contentTextView.text = saveData.object(forKey: "content") as? String
+            
+            number2 = Int(saveData.object(forKey: "day") as! String)!
+            
+        } else {
+        }
+        
+        //@IBAction func saveMemo(){
         
         //UserDefaultsに書き込み
         //saveData.set(contentTextView.text, forKey: "content")
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-@IBAction func plus(){
- number1 = number1 + 1
- dayLabel.text = String(number1)
-}
         
-@IBAction func minus(){
- number2 = number2 - 1
- dayLabel.text = String(number2)
-    
-}
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+        func plus(){
+            number1 = number1 + 1
+            dayLabel.text = String(number1)
+        }
+        
+        func minus(){
+            number2 = number2 - 1
+            dayLabel.text = String(number2)
+            
+        }
+    }
 }
